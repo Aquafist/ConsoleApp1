2b6e77df5c09ef2b74fab41a6686cc21bf321d52
Aquafist <shubhamagarwal0830@gmail.com>
2021-03-09 14:17:02 +0530

    poly

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        public void Shape()
+        {
+            Console.WriteLine("Area of given shape is:");
+        }
+        public int Shape(int a)
+        {
+            return a * a;
+        }
+        public double Shape(double a)
+        {
+            return a * a;
+        }
+        public int Shape(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Calculator
+{
+    public virtual void Solution()
+    {
+        Console.WriteLine("The solution of given equations");
+    }
+}
+class Addition:Calculator
+{
+    public override void Solution()
+    {
+        Console.Write("The sum of 2 and 5 is: ");
+        Console.WriteLine(2+5);
+    }
+}
+class Substract:Calculator
+{
+    public override void Solution()
+    {
+        Console.Write("The substraction of 9 and 3 is: ");
+        Console.WriteLine(9 - 3);
+    }
+}
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Polymorphism with override");
+            Calculator cal = new Calculator();
+            Calculator sum = new Addition();
+            Calculator sub = new Substract();
+            cal.Solution();
+            sum.Solution();
+            sub.Solution();
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Polymorphism with overloading");
+            Class1 area = new Class1();
+            area.Shape();
+            int sq1 = area.Shape(2);
+            Console.WriteLine("area of square with side = 2 is " + sq1);
+            double sq2 = area.Shape(2.3);
+            Console.WriteLine("area of square with side = 2.3 is " + sq2);
+            int rec = area.Shape(2,3);
+            Console.WriteLine("area of reactangle with side 2X3 is " + rec);
+        }
+    }
+}
